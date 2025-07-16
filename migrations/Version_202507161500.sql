e17f6769bd43732ac01390049e7643d8a5549ff1
Narasimha Daruru <narasimha.daruru@vacobinary.in>
2025-07-16 14:57:13 +0530

    Migration Updates
    Migration Changes

---
+++ migrations/Version_202507161500.sql
@@ -0,0 +1,7 @@
+ALTER TABLE employee_details
+  MODIFY COLUMN functional_focus_area VARCHAR(100),
+  MODIFY COLUMN highspring_division VARCHAR(100),
+  MODIFY COLUMN max_company_revenue_size VARCHAR(255),
+  MODIFY COLUMN industries_experience VARCHAR(255),
+  MODIFY COLUMN erp_software_experience VARCHAR(255),
+  MODIFY COLUMN max_work_hours_prefered DECIMAL(5,2);
